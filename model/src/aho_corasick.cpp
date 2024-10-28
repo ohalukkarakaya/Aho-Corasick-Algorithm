2084652839afa717e5b2d3e63acfaba638225135
HalukKarakaya4js <ozgurhaluk_krky@hotmail.com>
2024-10-28 23:14:38 +0100

    :construction: [New Method] 'build_failure_links' method

--- model/src/aho_corasick.cpp
+++ model/src/aho_corasick.cpp
@@ -2,6 +2,36 @@
 
 void AhoCorasick::build_failure_links()
 {
+    std::queue<TrieNode *> q;
+    root->fail = root;
+
+    // Failure links for nodes which is at root level is root
+    for (auto &[ch, child] : root->children)
+    {
+        child->fail = root;
+        q.push(child);
+    }
+
+    // Create Fail links with breadth-first search
+    while (!q.empty())
+    {
+        TrieNode *current = q.front();
+        q.pop();
+
+        for (auto &[ch, child] : current->children)
+        {
+            TrieNode *fail_node = current->fail;
+
+            while (fail_node != root && fail_node->children.find(ch) == fail_node->children.end())
+            {
+                fail_node = fail_node->fail;
+            }
+
+            if (fail_node->children.find(ch) != fail_node->children.end())
+            {
+            }
+        }
+    }
 }
 
 // Add word function
